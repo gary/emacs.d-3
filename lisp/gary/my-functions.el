e27f9a44e956fbb0614c99edda0af029997560f6
Gary Iams <ge.iams@gmail.com>
2016-02-05 22:51:57 -0500

    Bring over rest of utilty functions
    - https://github.com/gary/emacs-starter-kit/blob/personalizations/gary/defuns.el
    
    As of Emacs 24, swap-split and swap-windows are both broken. I may
    rewrite them at some point in the future.

--- lisp/gary/my-functions.el
+++ lisp/gary/my-functions.el
@@ -8,6 +8,21 @@
     (when (and (>= newalpha frame-alpha-lower-limit) (<= newalpha 100))
       (modify-frame-parameters nil (list (cons 'alpha newalpha))))))
 
+(defun rename-file-and-buffer (new-name)
+  "Renames both current buffer and file it's visiting to NEW-NAME."
+  (interactive "sNew name: ")
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not filename)
+        (message "Buffer '%s' is not visiting a file!" name)
+      (if (get-buffer new-name)
+          (message "A buffer named '%s' already exists!" new-name)
+        (progn
+          (rename-file name new-name 1)
+          (rename-buffer new-name)
+          (set-visited-file-name new-name)
+          (set-buffer-modified-p nil))))))
+
 (defun vi-open-next-line (arg)
   "Move to the next line (like vi) and then opens a line."
   (interactive "p")
