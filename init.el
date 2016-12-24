598615fb36626f987b4a4f134758ae942d7a4be0
Gary Iams <ge.iams@gmail.com>
2016-12-24 15:00:17 -0500

    Bootstrap use-package at init via git submodule

--- init.el
+++ init.el
@@ -1,13 +1,18 @@
+(require 'package)
+
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+             '("melpa" . "http://melpa.org/packages/"))
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (package-initialize)
-
-(require 'cask "/usr/local/share/emacs/site-lisp/cask/cask.el")
-(cask-initialize)
-
-(load-library "pallet")
-(pallet-mode t)
-
-(defconst my-custom-file
-      (f-join user-emacs-directory "lisp" user-login-name "my-custom.el"))
 
 (let ((default-directory "~/.emacs.d/lisp/"))
   (normal-top-level-add-subdirs-to-load-path))
+
+(eval-when-compile
+  (require 'use-package))
+
+(defconst my-custom-file
+  (concat user-emacs-directory "lisp/" user-login-name "/my-custom.el"))
--- lisp/gary/default.el
+++ lisp/gary/default.el
@@ -1,3 +1,9 @@
+;;; default.el --- Summary
+
+;;; Commentary:
+
+;;; Code:
+
 (defun set-font-for-host ()
   (setq ns-antialias-text t
         ns-input-font "Inconsolata"
@@ -12,29 +18,33 @@
 (setq browse-url-browser-function 'browse-url-default-macosx-browser
       mac-command-modifier        'meta ; command is alt in osx
       mac-option-modifier         'alt
-      ring-bell-function          'ignore)
+      ring-bell-function          'ignore
+      use-package-always-ensure   t)
 
-(require 'my-aliases)
-(require 'my-appearance)
-(require 'my-custom)
-(require 'my-edit-server)
-(require 'my-file-management)
-(require 'my-flycheck)
-(require 'my-functions)
-(require 'my-global-bindings)
-(require 'my-ido)
-(require 'my-lisp)
+;; (require 'my-aliases)
+;; (require 'my-appearance)
+;; (require 'my-custom)
+;; (require 'my-edit-server)
+;; (require 'my-file-management)
+;; (require 'my-flycheck)
+;; (require 'my-functions)
+;; (require 'my-global-bindings)
+;; (require 'my-ido)
+;; (require 'my-lisp)
 ;; (require 'my-magithub)
-(require 'my-misc)
-(require 'my-minibuffer)
-(require 'my-mode-mappings)
-(require 'my-prog-mode)
-(require 'my-projectile)
-(require 'my-registers)
-(require 'my-ruby)
-(require 'my-term)
-(require 'my-text-mode)
-(require 'my-utf8)
-(require 'my-web-mode)
-(require 'my-yagist)
-(require 'my-yasnippet)
+;; (require 'my-misc)
+;; (require 'my-minibuffer)
+;; (require 'my-mode-mappings)
+;; (require 'my-prog-mode)
+;; (require 'my-projectile)
+;; (require 'my-registers)
+;; (require 'my-ruby)
+;; (require 'my-term)
+;; (require 'my-text-mode)
+;; (require 'my-utf8)
+;; (require 'my-web-mode)
+;; (require 'my-yagist)
+;; (require 'my-yasnippet)
+
+(provide 'default)
+;;; default.el ends here
