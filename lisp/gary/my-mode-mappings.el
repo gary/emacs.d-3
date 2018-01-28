gary/emacs.d-3

commit 523409d27958ec465e6cea9461d2a23c47225821

Loosen Brewfile mode mapping, make it use ruby

lisp/gary/my-mode-mappings.el
@@ -1,21 +1,21 @@
 (mapc (lambda (auto-mode)
         (add-to-list 'auto-mode-alist auto-mode))
-      '(("Brewfile"        . conf-unix-mode)
-        (".builder$"       . enh-ruby-mode)
-        ("\\.gemspec$"     . enh-ruby-mode)
-        ("\\.podspec$"     . enh-ruby-mode)
-        ("\\.rake$"        . enh-ruby-mode)
-        ("\\.rb$"          . enh-ruby-mode)
-        ("\\.ru$"          . enh-ruby-mode)
-        ("\\.thor$"        . enh-ruby-mode)
+      '(("\\(\.?\\)Brewfile" . enh-ruby-mode)
+        (".builder$"         . enh-ruby-mode)
+        ("\\.gemspec$"       . enh-ruby-mode)
+        ("\\.podspec$"       . enh-ruby-mode)
+        ("\\.rake$"          . enh-ruby-mode)
+        ("\\.Rb$"            . enh-ruby-mode)
+        ("\\.ru$"            . enh-ruby-mode)
+        ("\\.thor$"          . enh-ruby-mode)
         ("/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\'" . enh-ruby-mode)
-        ("\\.mdwn$"        . markdown-mode)
-        ("\\.mdtxt$"       . markdown-mode)
-        ("\\.mkd$"         . markdown-mode)
-        ("\\.mkdn$"        . markdown-mode)
-        ("\\mdwn$"         . markdown-mode)
-        ("\\.zsh$"         . shell-script-mode)
-        ("\\.html?\\'"     . web-mode)
-        ("\\.erb$"         . web-mode)))
+        ("\\.mdwn$"          . markdown-mode)
+        ("\\.mdtxt$"         . markdown-mode)
+        ("\\.mkd$"           . markdown-mode)
+        ("\\.mkdn$"          . markdown-mode)
+        ("\\mdwn$"           . markdown-mode)
+        ("\\.zsh$"           . shell-script-mode)
+        ("\\.html?\\'"       . web-mode)
+        ("\\.erb$"           . web-mode)))
 
 (provide 'my-mode-mappings)

commit c1551d77d91af3461aba94c500a387588c2e815d

Restore enhanced ruby mode mapping
Broken in 523409d.

lisp/gary/my-mode-mappings.el
@@ -7,7 +7,7 @@
         ("\\.gemspec$"       . enh-ruby-mode)
         ("\\.podspec$"       . enh-ruby-mode)
         ("\\.rake$"          . enh-ruby-mode)
-        ("\\.Rb$"            . enh-ruby-mode)
+        ("\\.rb$"            . enh-ruby-mode)
         ("\\.ru$"            . enh-ruby-mode)
         ("\\.thor$"          . enh-ruby-mode)
         ("/\\(?:Gem\\|Rake\\|Cap\\|Thor\\|Vagrant\\|Guard\\|Pod\\)file\\'" . enh-ruby-mode)