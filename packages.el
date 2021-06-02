31c660815ad0a16ba3372bf1f3c3f3baaab71f25
Gary Iams <ge.iams@gmail.com>
2021-06-02 07:00:25 -0400

    Resurrect edit-server
    For use alongside firefox. Contrary to what I thought in be7662a,
    the project is alive and well and has been since 2010.

--- packages.el
+++ packages.el
@@ -72,6 +72,23 @@
   :config
   (setq ediff-window-setup-function 'ediff-setup-windows-plain))
 
+(use-package edit-server
+  :ensure t
+  :commands edit-server-start
+  :init (if after-init-time
+            (edit-server-start)
+          (add-hook 'after-init-hook
+                    #'(lambda() (edit-server-start))))
+  :config (setq edit-server-new-frame-alist
+                '((name . "Edit with Emacs FRAME")
+                  (top . 200)
+                  (left . 200)
+                  (width . 80)
+                  (height . 25)
+                  (minibuffer . t)
+                  (menu-bar-lines . t)
+                  (window-system . x))))
+
 (use-package enh-ruby-mode
   :interpreter "ruby"
   :mode (("\\(\.?\\)Brewfile" . enh-ruby-mode)
