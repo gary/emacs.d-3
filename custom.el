gary/emacs.d-3

commit c24ffdbda8728fa6fb4d7eb4d3a36383dcc95d80

Customization structural upgrade from emacs 27

custom.el
@@ -4,12 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   (quote
-    (ssh-agency ripgrep powershell coffee-mode hydra harvest ivy dumb-jump xkcd yard-mode yasnippet yaml-mode yagist wgrep-ag web-mode switch-window smex solarized-theme rubocop rspec-mode rbenv projectile paredit multi-term magithub markdown-mode kaesar js2-mode ido-completing-read+ gitignore-mode gitconfig-mode flx-ido flycheck-pos-tip flycheck feature-mode exec-path-from-shell enh-ruby-mode dictionary browse-kill-ring auto-package-update aggressive-indent ag use-package-ensure-system-package delight)))
- '(projectile-completion-system (quote ivy))
+   '(ssh-agency ripgrep powershell coffee-mode hydra harvest ivy dumb-jump xkcd yard-mode yasnippet yaml-mode yagist wgrep-ag web-mode switch-window smex solarized-theme rubocop rspec-mode rbenv projectile paredit multi-term magithub markdown-mode kaesar js2-mode ido-completing-read+ gitignore-mode gitconfig-mode flx-ido flycheck-pos-tip flycheck feature-mode exec-path-from-shell enh-ruby-mode dictionary browse-kill-ring auto-package-update aggressive-indent ag use-package-ensure-system-package delight))
+ '(projectile-completion-system 'ivy)
  '(term-bind-key-alist
-   (quote
-    (("C-a" . term-bol)
+   '(("C-a" . term-bol)
      ("C-c" . term-interrupt-subjob)
      ("C-p" . term-send-up)
      ("C-n" . term-send-down)
@@ -25,7 +23,7 @@
      ("M-p" . previous-line)
      ("M-n" . next-line)
      ("M-y" . yank-pop)
-     ("M-." . term-send-raw-meta))) t))
+     ("M-." . term-send-raw-meta)) t nil "Customized with use-package multi-term"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

commit 85f9967fbab15a22b51af5e5e55326fea3c66dd0

Restore multi-term customizations to c24ffdb
Accidentally removed in d1d6b46. Should be the last time this gets
inadvertently changed.

custom.el
@@ -23,7 +23,7 @@
      ("M-p" . previous-line)
      ("M-n" . next-line)
      ("M-y" . yank-pop)
-     ("M-." . term-send-raw-meta)) t))
+     ("M-." . term-send-raw-meta)) nil nil "Customized with use-package multi-term"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.