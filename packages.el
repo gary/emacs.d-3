372e9ab7f384a18e64d6507c9a617122496ffb0e
Gary Iams <ge.iams@gmail.com>
2023-10-18 16:19:49 -0400

    Use dtrt-indent to DWIM for per project indentation

--- packages.el
+++ packages.el
@@ -41,6 +41,11 @@
 
 (use-package coffee-mode)
 
+(use-package dtrt-indent
+  :custom
+  (dtrt-indent-global-mode t)
+  :delight)
+
 (use-package dictionary
   :init
   (bind-keys :prefix-map my-dictionary-prefix-map
