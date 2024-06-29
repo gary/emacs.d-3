59ceb23bc1e15e53d5f3d2011217bae2c3a0e509
Gary Iams <gary@users.noreply.github.com>
2024-06-29 16:06:44 -0400

    Stop using custom.el
    After realizing that this early decision (gary/dotfiles@3dc84094b688be3ab8fc3eb33910a5d73b866bb6)
    caused needless churn when I switched to use-package (0724cc4).
    
    Migrating (only) one unaccounted-for customization.

--- init.el
+++ init.el
@@ -26,9 +26,6 @@
   (define-inline emacs-path (path)
     (expand-file-name path user-emacs-directory)))
 
-(defconst my-custom-file
-  (emacs-path "custom.el"))
-
 (defconst tmp-directory
   (concat temporary-file-directory "/org.gnu.emacs"))
 
@@ -135,13 +132,16 @@
 (set-terminal-coding-system 'utf-8)
 (setq coding-system-for-write 'utf-8)
 
+;; faces
+(set-cursor-color "DeepPink1")
+
 ;; file management
 (setq abbrev-file-name (concat var-directory "abbrev_defs")
       auto-save-file-name-transforms `((".*" ,tmp-directory t))
       auto-save-list-file-prefix tmp-directory
       backup-by-copying-when-linked t
       backup-directory-alist `((".*" . ,tmp-directory))
-      custom-file my-custom-file
+      custom-file "custom.el"
       delete-old-versions t
       dired-clean-confirm-killing-deleted-buffers nil
       emacs-lock-default-locking-mode 'kill
@@ -150,7 +150,6 @@
       nsm-settings-file (concat var-directory "network-security.data")
       save-place-file (concat var-directory "places")
       version-control t)
-(load custom-file)
 
 ;; miscellaneous
 (add-to-list 'safe-local-variable-values '(lexical-binding . t))
