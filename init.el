gary/emacs.d-3

commit 9db23c719cb65a0c08aefb12f68422ec4dfd06b8

Stop using custom.el
After realizing that this early decision (gary/dotfiles@3dc84094b688be3ab8fc3eb33910a5d73b866bb6)
caused needless churn when I switched to use-package (0724cc4).

Migrating (only) one unaccounted-for customization.

init.el
@@ -26,9 +26,6 @@
   (define-inline emacs-path (path)
     (expand-file-name path user-emacs-directory)))
 
-(defconst my-custom-file
-  (emacs-path "custom.el"))
-
 (defconst tmp-directory
   (concat temporary-file-directory "/org.gnu.emacs"))
 
@@ -106,6 +103,7 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 
 ;; appearance
+(add-to-list 'default-frame-alist '(cursor-color . "DeepPink1"))
 (add-to-list 'default-frame-alist '(width . 173))
 (add-to-list 'default-frame-alist '(height . 81))
 (setq frame-title-format '(buffer-file-name "%f" ("%b")))
@@ -135,13 +133,16 @@
 (set-terminal-coding-system 'utf-8)
 (setq coding-system-for-write 'utf-8)
 
+;; faces
+(set-cursor-color "DeepPink1")
+
 ;; file management
 (setq abbrev-file-name (concat var-directory "abbrev_defs")
       auto-save-file-name-transforms `((".*" ,tmp-directory t))
       auto-save-list-file-prefix tmp-directory
       backup-by-copying-when-linked t
       backup-directory-alist `((".*" . ,tmp-directory))
-      custom-file my-custom-file
+      custom-file "custom.el"
       delete-old-versions t
       dired-clean-confirm-killing-deleted-buffers nil
       emacs-lock-default-locking-mode 'kill
@@ -150,7 +151,6 @@
       nsm-settings-file (concat var-directory "network-security.data")
       save-place-file (concat var-directory "places")
       version-control t)
-(load custom-file)
 
 ;; miscellaneous
 (add-to-list 'safe-local-variable-values '(lexical-binding . t))

.gitignore
@@ -1,2 +1,3 @@
+/custom.el
 /var/
 /tree-sitter/

custom.el
@@ -1,35 +1,0 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(dtrt-indent vterm x509-mode cmake-mode graphql-mode docker kubernetes lsp-mode forge ox-jira dockerfile-mode edit-server groovy-mode json-reformat json-snatcher json-mode kotlin-mode ssh-agency ripgrep powershell coffee-mode hydra harvest ivy dumb-jump xkcd yard-mode yasnippet yaml-mode yagist wgrep-ag web-mode switch-window smex solarized-theme rubocop rspec-mode rbenv projectile paredit multi-term magithub markdown-mode kaesar js2-mode ido-completing-read+ git-modes flx-ido flycheck-pos-tip flycheck feature-mode exec-path-from-shell enh-ruby-mode dictionary browse-kill-ring auto-package-update aggressive-indent ag use-package-ensure-system-package delight))
- '(projectile-completion-system 'ivy)
- '(term-bind-key-alist
-   '(("C-a" . term-bol)
-     ("C-c" . term-interrupt-subjob)
-     ("C-p" . term-send-up)
-     ("C-n" . term-send-down)
-     ("C-s" . isearch-forward)
-     ("C-r" . term-send-reverse-search-history)
-     ("C-m" . term-send-raw)
-     ("C-k" . term-send-kill-whole-line)
-     ("C-y" . term-paste)
-     ("C-_" . term-send-raw)
-     ("M-f" . term-send-forward-word)
-     ("M-b" . term-send-backward-word)
-     ("M-K" . term-send-kill-line)
-     ("M-p" . previous-line)
-     ("M-n" . next-line)
-     ("M-y" . yank-pop)
-     ("M-." . term-send-raw-meta)) nil nil "Customized with use-package multi-term"))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(cursor ((t (:background "DeepPink1" :foreground "DeepPink4" :inverse-video t))))
- '(term-color-blue ((t (:background "DeepSkyBlue3" :foreground "DeepSkyBlue3")))))
-
-(provide 'my-custom)

commit d9c50f1a3d5af93752067e6ec9a350b640a749c5

Contain customizations in Emacs config
Inadvertently setting custom-file to a relative value created the
(very) unwanted side effect of dropping custom.el files in the current
working directory.

Fixes bug introduced by 9db23c7.

init.el
@@ -147,7 +147,7 @@
       auto-save-list-file-prefix data-directory
       backup-by-copying-when-linked t
       backup-directory-alist `((".*" . ,data-directory))
-      custom-file "custom.el"
+      custom-file (emacs-path "custom.el")
       delete-old-versions t
       dired-clean-confirm-killing-deleted-buffers nil
       emacs-lock-default-locking-mode 'kill